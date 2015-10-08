7d7a9bc31622635451066d519eec38e53507b5a2
NAEL1 <elhoufi@correo.ugr.es>
2015-10-08 21:49:08 +0200

    practica1

--- practica1/ejecuciones_ordenacion.sh
+++ practica1/ejecuciones_ordenacion.sh
@@ -3,9 +3,9 @@
 declare -i inicio=100
 declare -i fin=30000
 declare -i incremento=500
-
 declare -i i=$inicio
-
+ sudo touch t_ordenacion.dat
 while(($i < $fin));do
 	echo `./ordenacion $i 10000` >> t_ordenacion.dat
+	let i+=$incremento
 done
